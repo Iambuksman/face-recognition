d6ea7d33ad675a6c7aace1386f43c1b28a2fded2
iambuksman <iambuksman@gmail.com>
2020-02-18 17:23:06 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,39 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
+import Particles from 'react-particles-js';
 import './App.css';
+import Navigation from './Components/Navigation/Navigation';
+import Logo from './Components/Logo/Logo';
+import ImageLinkForm from './Components/ImageLinkForm/ImageLinkForm';
+import Rank from './Components/Rank/Rank';
+// import { render } from '@testing-library/react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+const ParticlesOptions = {
+    particules: {
+      number: {
+        value: 30,
+        density: {
+          enable: true,
+          value_area: 800
+        }
+      }
+    }
+}
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Particles className='particules'
+          params={ParticlesOptions}
+        />
+        <Navigation />
+        <Logo/>
+        <Rank />
+        <ImageLinkForm/>
+        {/* <FaceRecognition/> */}
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ src/Components/ImageLinkForm/ImageLinkForm.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import './ImageLinkForm.css';
+
+const ImageLinkForm = () => {
+    return (
+        <div>
+            <p className='f3'>
+                {'This magic Brain will detect faces in your pictures. Give it a trial.'}
+            </p>
+            <div className='center'>
+                <div className='form center pa4 br3 shadow-5'>
+                <input className='f4 pa2 w-70 center' type='tex'/>
+                    <button className='w-30 grow  f4 link ph3 pv2 dib pointer white bg-black'><text style={{color: 'white'}}>D E T E C T</text></button>
+                </div>
+            </div>
+        </div>
+    );
+}
+export default ImageLinkForm; 
